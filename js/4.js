ff47695b83563560c7492f53f8a9de799319965e
“chaeun” <latte809@gmail.com>
2022-04-23 18:13:36 +0900

    10th commit

--- js/4.js
+++ js/4.js
@@ -13,8 +13,8 @@ setInterval(typing, 170)
 
 function typing1() {
 	document.getElementById('txt2').style.display = "block"
-	document.getElementById('click1mo').style.border = '0.1em solid transparent'
-	document.getElementById('click2mo').style.border = '0.1em solid white'
+	document.getElementById('click1').style.border = '0.1em solid transparent'
+	document.getElementById('click2').style.border = '0.1em solid white'
 	const content = "크다가, 작다가, 뾰족하다가, 동그랗다가.       "
 	const text = document.querySelector("#text2")
 	let index = 0;
@@ -32,8 +32,8 @@ function typing1() {
 
 function typing2() {
 	document.getElementById('txt3').style.display = "block"
-	document.getElementById('click2mo').style.border = '0.1em solid transparent'
-	document.getElementById('click3mo').style.border = '0.1em solid white'
+	document.getElementById('click2').style.border = '0.1em solid transparent'
+	document.getElementById('click3').style.border = '0.1em solid white'
 	const content = "제멋대로긴 한데, 그릇에 맞게 형태를 잘 맞추잖아.       "
 	const text = document.querySelector("#text3")
 	let index = 0;
@@ -49,8 +49,8 @@ function typing2() {
 
 function typing3() {
 	document.getElementById('txt4').style.display = "block"
-	document.getElementById('click3mo').style.border = '0.1em solid transparent'
-	document.getElementById('click4mo').style.border = '0.1em solid white'
+	document.getElementById('click3').style.border = '0.1em solid transparent'
+	document.getElementById('click4').style.border = '0.1em solid white'
 	const content = "그게 다 성질이 명확하니까 가능한 거지.       "
 	const text = document.querySelector("#text4")
 	let index = 0;
