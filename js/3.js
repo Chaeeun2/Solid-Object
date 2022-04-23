1556e2fdebbb1b2a3f4124834f0f76b9759e78f7
“chaeun” <latte809@gmail.com>
2022-04-23 16:02:15 +0900

    7th commit

--- js/3.js
+++ js/3.js
@@ -1,104 +1,234 @@
+var windowWidth = window.matchMedia("screen and (max-width: 900px)");
+
+if (windowWidth.matches) {
+	//screen and (max-width: 768px)에 해당한다.
 	const content = "물은 차갑게 얼 때 더 조심해야 돼.       "
 	const text = document.querySelector("#text1")
 	let index = 0;
 
-	function typing(){
+	function typing() {
 		text.textContent += content[index++]
-		if(index > content.length){
+		if (index > content.length) {
 			text.textContent = "걔는 차갑게 굴 때 더 조심해야 돼. "
 		}
 	}
 	setInterval(typing, 170)
 
 
-function typing1(){
+	function typing1() {
 
-	document.getElementById('body').style.background = "url('https://cdn.imweb.me/upload/S2022030222ee0b2e93de0/e4fbaf7d79823.jpg')"
-	document.getElementById('text2').style.display = "block"
-	document.getElementById('click1').style.display = "none"
-	document.getElementById('click2').style.display = "block"
-	const content = "평소엔 되게 유해보이잖아.       "
-	const text = document.querySelector("#text2")
+		document.getElementById('body').style.background = "url('https://cdn.imweb.me/upload/S2022030222ee0b2e93de0/8cb82f76d282e.jpg')"
+		document.getElementById('body').style.backgroundSize = "cover"
+		document.getElementById('body').style.backgroundPosition = "center top"
+		document.getElementById('body').style.backgroundRepeat = "no-repeat"
+		document.getElementById('text2').style.display = "block"
+		document.getElementById('click1').style.display = "none"
+		document.getElementById('click2').style.display = "block"
+		const content = "평소엔 되게 유해보이잖아.       "
+		const text = document.querySelector("#text2")
+		let index = 0;
+
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "평소엔 되게 착해보이잖아."
+			}
+		}
+		setInterval(typing, 170)
+
+	}
+
+
+	function typing2() {
+
+		document.getElementById('body').style.background = "url('https://cdn.imweb.me/upload/S2022030222ee0b2e93de0/ffcbf5143073c.jpg')"
+		document.getElementById('body').style.backgroundSize = "cover"
+		document.getElementById('body').style.backgroundPosition = "center top"
+		document.getElementById('body').style.backgroundRepeat = "no-repeat"
+		document.getElementById('text3').style.display = "block"
+		document.getElementById('click2').style.display = "none"
+		document.getElementById('click3').style.display = "block"
+		const content = "근데 차가울 땐 진짜 딱딱하거든.       "
+		const text = document.querySelector("#text3")
+		let index = 0;
+
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "근데 차가울 땐 진짜 까칠하거든."
+			}
+		}
+		setInterval(typing, 170)
+
+	}
+
+
+	function typing3() {
+
+		document.getElementById('body').style.background = "url('https://cdn.imweb.me/upload/S2022030222ee0b2e93de0/1d6c4dd98f7d6.jpg')"
+		document.getElementById('body').style.backgroundSize = "cover"
+		document.getElementById('body').style.backgroundPosition = "center top"
+		document.getElementById('body').style.backgroundRepeat = "no-repeat"
+		document.getElementById('text4').style.display = "block"
+		document.getElementById('click3').style.display = "none"
+		document.getElementById('click4').style.display = "block"
+		const content = "그럴 땐 함부로 건드리지 말고, 그냥 가만히 둬.       "
+		const text = document.querySelector("#text4")
+		let index = 0;
+
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "그럴 땐 함부로 자극하지 말고, 그냥 혼자 냅둬."
+			}
+		}
+		setInterval(typing, 170)
+
+	}
+
+
+	function typing4() {
+
+		document.getElementById('body').style.background = "#2224f4"
+		document.getElementById('text5').style.display = "block"
+		document.getElementById('click4').style.display = "none"
+		document.getElementById('click5').style.display = "block"
+		const content = "가만히 두면 금방 원래대로 돌아오니까.       "
+		const text = document.querySelector("#text5")
+		let index = 0;
+
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "혼자 냅두면 금방 차분하게 돌아오니까."
+			}
+		}
+		setInterval(typing, 170)
+
+	}
+
+
+
+	function typing5() {
+
+		location.href = "4.html"
+
+	}
+
+}
+
+else {
+	const content = "물은 차갑게 얼 때 더 조심해야 돼.       "
+	const text = document.querySelector("#text1")
 	let index = 0;
 
-	function typing(){
+	function typing() {
 		text.textContent += content[index++]
-		if(index > content.length){
-			text.textContent = "평소엔 되게 착해보이잖아."
+		if (index > content.length) {
+			text.textContent = "걔는 차갑게 굴 때 더 조심해야 돼. "
 		}
 	}
 	setInterval(typing, 170)
 
-}
 
+	function typing1() {
 
-function typing2(){
+		document.getElementById('body').style.background = "url('https://cdn.imweb.me/upload/S2022030222ee0b2e93de0/e4fbaf7d79823.jpg')"
+		document.getElementById('body').style.backgroundSize = "cover"
+		document.getElementById('body').style.backgroundPosition = "center top"
+		document.getElementById('body').style.backgroundRepeat = "no-repeat"
+		document.getElementById('text2').style.display = "block"
+		document.getElementById('click1').style.display = "none"
+		document.getElementById('click2').style.display = "block"
+		const content = "평소엔 되게 유해보이잖아.       "
+		const text = document.querySelector("#text2")
+		let index = 0;
 
-	document.getElementById('body').style.background = "url('https://cdn.imweb.me/upload/S2022030222ee0b2e93de0/5059daf93f976.jpg')"
-	document.getElementById('text3').style.display = "block"
-	document.getElementById('click2').style.display = "none"
-	document.getElementById('click3').style.display = "block"
-	const content = "근데 차가울 땐 진짜 딱딱하거든.       "
-	const text = document.querySelector("#text3")
-	let index = 0;
-
-	function typing(){
-		text.textContent += content[index++]
-		if(index > content.length){
-			text.textContent = "근데 차가울 땐 진짜 까칠하거든."
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "평소엔 되게 착해보이잖아."
+			}
 		}
+		setInterval(typing, 170)
+
 	}
-	setInterval(typing, 170)
-
-}
 
 
-function typing3(){
+	function typing2() {
 
-	document.getElementById('body').style.background = "url('https://cdn.imweb.me/upload/S2022030222ee0b2e93de0/946ef8a751b87.jpg')"
-	document.getElementById('text4').style.display = "block"
-	document.getElementById('click3').style.display = "none"
-	document.getElementById('click4').style.display = "block"
-	const content = "그럴 땐 함부로 건드리지 말고, 그냥 가만히 둬.       "
-	const text = document.querySelector("#text4")
-	let index = 0;
+		document.getElementById('body').style.background = "url('https://cdn.imweb.me/upload/S2022030222ee0b2e93de0/5059daf93f976.jpg')"
+		document.getElementById('body').style.backgroundSize = "cover"
+		document.getElementById('body').style.backgroundPosition = "center top"
+		document.getElementById('body').style.backgroundRepeat = "no-repeat"
+		document.getElementById('text3').style.display = "block"
+		document.getElementById('click2').style.display = "none"
+		document.getElementById('click3').style.display = "block"
+		const content = "근데 차가울 땐 진짜 딱딱하거든.       "
+		const text = document.querySelector("#text3")
+		let index = 0;
 
-	function typing(){
-		text.textContent += content[index++]
-		if(index > content.length){
-			text.textContent = "그럴 땐 함부로 자극하지 말고, 그냥 혼자 냅둬."
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "근데 차가울 땐 진짜 까칠하거든."
+			}
 		}
+		setInterval(typing, 170)
+
 	}
-	setInterval(typing, 170)
-
-}
 
 
-function typing4(){
+	function typing3() {
 
-	document.getElementById('body').style.background = "#2224f4"
-	document.getElementById('text5').style.display = "block"
-	document.getElementById('click4').style.display = "none"
-	document.getElementById('click5').style.display = "block"
-	const content = "가만히 두면 금방 원래대로 돌아오니까.       "
-	const text = document.querySelector("#text5")
-	let index = 0;
+		document.getElementById('body').style.background = "url('https://cdn.imweb.me/upload/S2022030222ee0b2e93de0/946ef8a751b87.jpg')"
+		document.getElementById('body').style.backgroundSize = "cover"
+		document.getElementById('body').style.backgroundPosition = "center top"
+		document.getElementById('body').style.backgroundRepeat = "no-repeat"
+		document.getElementById('text4').style.display = "block"
+		document.getElementById('click3').style.display = "none"
+		document.getElementById('click4').style.display = "block"
+		const content = "그럴 땐 함부로 건드리지 말고, 그냥 가만히 둬.       "
+		const text = document.querySelector("#text4")
+		let index = 0;
 
-	function typing(){
-		text.textContent += content[index++]
-		if(index > content.length){
-			text.textContent = "혼자 냅두면 금방 차분하게 돌아오니까."
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "그럴 땐 함부로 자극하지 말고, 그냥 혼자 냅둬."
+			}
 		}
+		setInterval(typing, 170)
+
 	}
-	setInterval(typing, 170)
+
+
+	function typing4() {
+
+		document.getElementById('body').style.background = "#2224f4"
+		document.getElementById('text5').style.display = "block"
+		document.getElementById('click4').style.display = "none"
+		document.getElementById('click5').style.display = "block"
+		const content = "가만히 두면 금방 원래대로 돌아오니까.       "
+		const text = document.querySelector("#text5")
+		let index = 0;
+
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "혼자 냅두면 금방 차분하게 돌아오니까."
+			}
+		}
+		setInterval(typing, 170)
+
+	}
+
+
+
+	function typing5() {
+
+		location.href = "4.html"
+
+	}
 
 }
-
-
-
-function typing5(){
-
-location.href="4.html"
-
-}
-
--- js/4.js
+++ js/4.js
@@ -1,71 +1,153 @@
+var windowWidth = window.matchMedia("screen and (max-width: 900px)");
 
-const content = "근데 나는 물 좋아해.       "
-const text = document.querySelector("#text1")
-let index = 0;
-
-function typing(){
-	text.textContent += content[index++]
-	if(index > content.length){
-		text.textContent = "근데 나는 걔 좋아해."
-	}
-}
-setInterval(typing, 170)
-
-
-
-function typing1(){
-	document.getElementById('txt2').style.display = "block"
-	document.getElementById('click1').style.border = '0.1em solid transparent'
-	document.getElementById('click2').style.border = '0.1em solid white'
-	const content = "크다가, 작다가, 뾰족하다가, 동그랗다가.       "
-	const text = document.querySelector("#text2")
+if (windowWidth.matches) {
+	const content = "근데 나는 물 좋아해.       "
+	const text = document.querySelector("#text1")
 	let index = 0;
 
-	function typing(){
+	function typing() {
 		text.textContent += content[index++]
-		if(index > content.length){
-			text.textContent = "까칠하다가, 유하다가, 나서다가, 조용하다가."
+		if (index > content.length) {
+			text.textContent = "근데 나는 걔 좋아해."
 		}
 	}
 	setInterval(typing, 170)
 
+
+
+	function typing1() {
+		document.getElementById('txt2').style.display = "block"
+		document.getElementById('click1mo').style.border = '0.1em solid transparent'
+		document.getElementById('click2mo').style.border = '0.1em solid white'
+		const content = "크다가, 작다가, 뾰족하다가, 동그랗다가.       "
+		const text = document.querySelector("#text2")
+		let index = 0;
+
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "까칠하다가, 유하다가, 나서다가, 조용하다가."
+			}
+		}
+		setInterval(typing, 170)
+
+	}
+
+
+	function typing2() {
+		document.getElementById('txt3').style.display = "block"
+		document.getElementById('click2mo').style.border = '0.1em solid transparent'
+		document.getElementById('click3mo').style.border = '0.1em solid white'
+		const content = "제멋대로긴 한데, 그릇에 맞게 형태를 잘 맞추잖아.       "
+		const text = document.querySelector("#text3")
+		let index = 0;
+
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "제멋대로긴 한데, 상황에 맞게 성격을 잘 맞추잖아."
+			}
+		}
+		setInterval(typing, 170)
+	}
+
+	function typing3() {
+		document.getElementById('txt4').style.display = "block"
+		document.getElementById('click3mo').style.border = '0.1em solid transparent'
+		document.getElementById('click4mo').style.border = '0.1em solid white'
+		const content = "그게 다 성질이 명확하니까 가능한 거지.       "
+		const text = document.querySelector("#text4")
+		let index = 0;
+
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "그게 다 내면이 단단하니까 가능한 거지."
+			}
+		}
+		setInterval(typing, 170)
+	}
+
+	function typing4() {
+		location.href = "5.html"
+	}
+
+
+
 }
 
-
-function typing2(){
-	document.getElementById('txt3').style.display = "block"
-	document.getElementById('click2').style.border = '0.1em solid transparent'
-	document.getElementById('click3').style.border = '0.1em solid white'
-	const content = "제멋대로긴 한데, 그릇에 맞게 형태를 잘 맞추잖아.       "
-	const text = document.querySelector("#text3")
+else {
+	const content = "근데 나는 물 좋아해.       "
+	const text = document.querySelector("#text1")
 	let index = 0;
 
-	function typing(){
+	function typing() {
 		text.textContent += content[index++]
-		if(index > content.length){
-			text.textContent = "제멋대로긴 한데, 상황에 맞게 성격을 잘 맞추잖아."
+		if (index > content.length) {
+			text.textContent = "근데 나는 걔 좋아해."
 		}
 	}
 	setInterval(typing, 170)
-}
 
-function typing3(){
-	document.getElementById('txt4').style.display = "block"
-	document.getElementById('click3').style.border = '0.1em solid transparent'
-	document.getElementById('click4').style.border = '0.1em solid white'
-	const content = "그게 다 성질이 명확하니까 가능한 거지.       "
-	const text = document.querySelector("#text4")
-	let index = 0;
 
-	function typing(){
-		text.textContent += content[index++]
-		if(index > content.length){
-			text.textContent = "그게 다 내면이 단단하니까 가능한 거지."
+
+	function typing1() {
+		document.getElementById('txt2').style.display = "block"
+		document.getElementById('click1').style.border = '0.1em solid transparent'
+		document.getElementById('click2').style.border = '0.1em solid white'
+		const content = "크다가, 작다가, 뾰족하다가, 동그랗다가.       "
+		const text = document.querySelector("#text2")
+		let index = 0;
+
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "까칠하다가, 유하다가, 나서다가, 조용하다가."
+			}
 		}
-	}
-	setInterval(typing, 170)
-}
+		setInterval(typing, 170)
 
-function typing4(){
-	location.href="5.html"
+	}
+
+
+	function typing2() {
+		document.getElementById('txt3').style.display = "block"
+		document.getElementById('click2').style.border = '0.1em solid transparent'
+		document.getElementById('click3').style.border = '0.1em solid white'
+		const content = "제멋대로긴 한데, 그릇에 맞게 형태를 잘 맞추잖아.       "
+		const text = document.querySelector("#text3")
+		let index = 0;
+
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "제멋대로긴 한데, 상황에 맞게 성격을 잘 맞추잖아."
+			}
+		}
+		setInterval(typing, 170)
+	}
+
+	function typing3() {
+		document.getElementById('txt4').style.display = "block"
+		document.getElementById('click3').style.border = '0.1em solid transparent'
+		document.getElementById('click4').style.border = '0.1em solid white'
+		const content = "그게 다 성질이 명확하니까 가능한 거지.       "
+		const text = document.querySelector("#text4")
+		let index = 0;
+
+		function typing() {
+			text.textContent += content[index++]
+			if (index > content.length) {
+				text.textContent = "그게 다 내면이 단단하니까 가능한 거지."
+			}
+		}
+		setInterval(typing, 170)
+	}
+
+	function typing4() {
+		location.href = "5.html"
+	}
+
+
+
 }
